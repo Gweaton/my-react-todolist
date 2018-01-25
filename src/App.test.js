41b328b876598c612fa22641b6508a0150ef8487
George Eaton <george_eaton@hotmail.co.uk>
2018-01-25 14:15:12 +0000

    Refactor App tests to use enzyme pure function

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,14 @@
+/* global it, expect */
+
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
+import { shallow } from 'enzyme';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  const component = shallow(<App />);
+  expect(component.exists()).toEqual(true);
 });
